760d1adf7ec55f1a5ba518cd0862fb2cf09ed118
Katarzyna Lachcik <kaisek@poczta.fm>
2017-06-25 20:37:04 +0100

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function drawTree(height) {
+	for(var i = 1; i <= height; i++) {
+		var star = '';
+			for (var j = 1; j <= i; j++) { 
+			star+= '*';
+			};
+			console.log(star);
+	};
+};
+drawTree(5);
