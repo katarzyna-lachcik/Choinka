a9726e2c427a64841025c36c5aae9b43b2def78f
Katarzyna Lachcik <kaisek@poczta.fm>
2017-06-26 14:45:35 +0100

    tabs

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@ function drawTree(height) {
 	for(var i = 1; i <= height; i++) {
 		var star = '';
 			for (var j = 1; j <= i; j++) { 
-			star+= '*';
+				star += '*';
 			};
-			console.log(star);
+	console.log(star);
 	};
 };
 drawTree(5);
